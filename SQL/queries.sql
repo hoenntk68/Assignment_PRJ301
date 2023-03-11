6fa68fc0ff4421e3365bdaf0a199b2e2807dce33
hoenntk68 <hn8319542@gmail.com>
2023-03-12 02:23:57 +0700

    add procedures

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -2,6 +2,7 @@ use PRJ301_TakeAttendanceSystem
 
 
 /*
+---------------------------------GET WEEKLY SCHEDULE---------------------------------
 This procedure returns information about an instructor's schedule in a given week.
 It takes 2 parameters: the date on which the week start and the instructor's ID
 */
@@ -11,17 +12,21 @@ create proc getWeeklySchedule
 @instructorId varchar(20)
 as
 begin
-	select * from Session s join [Group] g on s.groupId = g.groupId
+	select sessionId, sessionName, date, slotNumber, startTime, endTime, instructorId,roomId, groupName, courseId
+	from Session s 
+	join [Group] g on s.groupId = g.groupId
+	join TimeSlot ts on ts.slotId = s.slotId
 	where 
 		s.date between @startWeek and DATEADD(day, 6, @startWeek)
 		and g.instructorId = @instructorId
 	order by s.date
 end
 --drop proc getWeeklySchedule
---exec getWeeklySchedule '3/1/2023', 'sonnt5'
+--exec getWeeklySchedule '2023-3-1', 'sonnt5'
 
 
 /*
+---------------------------------GET SESSION REPORT---------------------------------
 This procedure returns information about a session's attendance information.
 It takes 1 parameters: the id of the session
 */ go
@@ -40,6 +45,7 @@ end
 
 
 /*
+---------------------------------GET GROUP REPORT---------------------------------
 This procedure returns information about a group's attendance information.
 It takes 1 parameters: the id of the session
 */ go
@@ -48,14 +54,167 @@ create proc getGroupReport
 as
 begin
 	declare @absent int
-	--set @absent = select count
-	select a.studentId, stu.studentName, stu.studentImage, a.status
+	select a.sessionId, a.studentId, stu.studentName, stu.studentImage, a.status
 	from Attend a join Session s on a.sessionId = s.sessionId
-	--join [Group] g on s.groupId = g.groupId 
 	join Student stu on a.studentId = stu.studentId
-	where s.groupId = @groupId
+	group by a.sessionId, s.groupId, a.studentId, stu.studentName, stu.studentImage, a.status
+	having s.groupId = @groupId
 end
 
 --drop proc getGroupReport
---exec getGroupReport 13
+--exec getGroupReport 15
 
+/*
+---------------------------------GET STUDENTS FROM GROUPID---------------------------------
+This procedure returns a list of students from given groupId
+*/
+go 
+create proc getStudentsFromGroup
+@groupId int
+as
+begin
+	select s.studentId, s.studentImage, s.studentName
+	from Student s
+	join Participate p on s.studentId = p.studentId
+	join [Group] g on g.groupId = p.groupId
+	where p.groupId = @groupId
+end
+
+-- drop proc getStudentsFromGroup
+-- exec getStudentsFromGroup 15
+
+/*
+---------------------------------GET STUDENTS FROM SESSIONID---------------------------------
+This procedure returns a list of students from given sessionId
+*/
+go 
+create proc getStudentsFromSession
+@sessionId int
+as
+begin
+	select s.studentId, s.studentImage, s.studentName
+	from Student s
+	join Participate p on s.studentId = p.studentId
+	join [Group] g on g.groupId = p.groupId
+	join Session ses on ses.groupId = g.groupId
+	where ses.sessionId = @sessionId
+end
+
+-- drop proc getStudentsFromSession
+-- exec getStudentsFromSession 40
+
+/*
+---------------------------------GET REPORT FROM SESSIONID---------------------------------
+Show attendance record from a given sessionId
+*/
+
+/*go 
+create proc getSessionReport
+@sessionId int
+as
+begin
+	select s.studentId, s.studentImage, s.studentName, a.status
+	from Student s join Attend a on s.studentId = a.studentId
+	join Session ses on a.sessionId = ses.sessionId
+	where ses.sessionId = @sessionId
+end*/
+
+-- drop proc getSessionReport
+-- exec getSessionReport 40
+
+/*
+---------------------------------GET GROUP FROM GROUPID---------------------------------
+Return groupName from given groupId
+*/
+go
+create proc getGroup
+@groupId int
+as 
+begin
+select * from [Group] g where g.groupId = @groupId
+end
+
+-- drop proc getGroup
+-- exec getGroup 10
+
+
+select count(s.sessionId) as noOfSessions, s.groupId, g.courseId
+from Session s join [Group] g on s.groupId = g.groupId
+where g.groupId in (10, 11, 12, 13, 14, 15)
+group by s.groupId, g.courseId
+
+/*
+---------------------------------GET NUMBER OF SESSIONS FROM GROUPID---------------------------------
+Return groupName from given groupId
+*/
+go
+create proc getSessionCount
+@groupId int
+as 
+begin
+	select count(s.sessionId) as noOfSessions, s.groupId, g.courseId
+	from Session s join [Group] g on s.groupId = g.groupId
+	where g.groupId = @groupId
+	group by s.groupId, g.courseId
+end
+
+/*
+---------------------------------GET NUMBER OF STUDENTS FROM GROUPID---------------------------------
+Return groupName from given groupId
+*/
+go
+create proc getStudentCount
+@groupId int
+as 
+begin
+	select g.groupId, g.groupName, g.courseId, count(s.studentId) as noOfStudents
+	from Student s join Participate p on s.studentId = p .studentId
+	join [Group] g on g.groupId = p.groupId
+	where g.groupId = @groupId
+	group by g.groupId, g.groupName, g.courseId
+end
+
+-- drop proc getStudentCount
+-- exec getStudentCount 15
+
+/*
+---------------------------------GET SESSIONS FROM GROUPID---------------------------------
+Return groupName from given groupId
+*/
+go
+create proc getSessionsFromGroup
+@groupId int
+as 
+begin
+	select s.sessionId, s.date, s.groupId
+	from Session s
+	where s.groupId = @groupId
+end
+
+-- drop proc getSessionsFromGroup
+-- exec getSessionsFromGroup 11
+
+
+/*
+---------------------------------GET ABSENT PERCENTAGE FROM GROUPID & STUDENTID---------------------------------
+Return groupName from given groupId
+*/
+go
+create proc getAbsentPercentage
+@groupId int,
+@studentId varchar(8)
+as 
+begin
+	select s.sessionId, s.date, s.groupId
+	from Session s
+	where s.groupId = @groupId
+end
+
+-- drop proc getAbsentPercentage
+-- exec getAbsentPercentage 11
+
+select 
+g.groupId, g.groupName, g.courseId, c.courseName, g.instructorId, i.instructorName
+from [Group] g
+join Course c on g.courseId = c.courseId
+join Instructor i on g.instructorId = i.instructorId
